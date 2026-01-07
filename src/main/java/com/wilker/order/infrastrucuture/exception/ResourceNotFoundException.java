e3c60e3f1af494974fd30225819e6481a3900e45
Wilker Sampaio <neoxextreme@gmail.com>
2026-01-06 20:24:38 -0400

    feature: implementado exceçoes

---
+++ src/main/java/com/wilker/order/infrastrucuture/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.wilker.order.infrastrucuture.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/wilker/order/infrastrucuture/exception/StatusException.java
@@ -0,0 +1,7 @@
+package com.wilker.order.infrastrucuture.exception;
+
+public class StatusException extends RuntimeException {
+    public StatusException(String message) {
+        super(message);
+    }
+}
